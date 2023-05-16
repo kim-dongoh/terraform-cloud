5769956ef5c5474abc3212422424f28d09495967
kim-dongoh <dongoh31277@gmail.com>
2023-05-16 23:54:40 +0900

    SG Test

--- terraform.tf
+++ terraform.tf
@@ -17,3 +17,52 @@ terraform {
 
   required_version = ">= 1.1.0"
 }
+
+resource "aws_security_group" "do_kim-TFE" {
+  name = "do_kim-TFE"
+  tags = {
+    Name = "do_kim-TFE"
+  }
+
+  #  ingress {
+  #    from_port   = 22
+  #    to_port     = 22
+  #    protocol    = "tcp"
+  #    cidr_blocks = ["0.0.0.0/0"]
+  #  }
+  #
+  #  ingress {
+  #    from_port   = 8800
+  #    to_port     = 8800
+  #    protocol    = "tcp"
+  #    cidr_blocks = ["0.0.0.0/0"]
+  #  }
+  #
+  #  ingress {
+  #    from_port   = 80
+  #    to_port     = 80
+  #    protocol    = "tcp"
+  #    cidr_blocks = ["0.0.0.0/0"]
+  #  }
+  #
+  #  ingress {
+  #    from_port   = 443
+  #    to_port     = 443
+  #    protocol    = "tcp"
+  #    cidr_blocks = ["0.0.0.0/0"]
+  #  }
+
+  ingress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+}
